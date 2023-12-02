7c549634fceabaf32c4032aecf49f5b0213f550b
Kirill Evrov <kirillevrov@gmail.com>
2023-12-02 13:05:51 +0300

    remove source maps in production mode

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,5 +2,13 @@ const path = require("path");
 const { copyLibFiles } = require('@builder.io/partytown/utils');
 
 exports.onPreBuild = async () => {
-  await copyLibFiles(path.join(__dirname, "static", "~partytown"));
+    await copyLibFiles(path.join(__dirname, "static", "~partytown"));
+};
+
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+    if (getConfig().mode === 'production') {
+        actions.setWebpackConfig({
+            devtool: false
+        });
+    }
 };
