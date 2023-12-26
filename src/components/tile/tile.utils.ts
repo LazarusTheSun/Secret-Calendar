392a55473f2aa39a735c9de1eefcce574483cea0
Kirill Evrov <kirillevrov@gmail.com>
2023-12-26 22:24:50 +0300

    fix tiles

--- src/components/tile/tile.utils.ts
+++ src/components/tile/tile.utils.ts
@@ -1,7 +1,7 @@
 import { CHECK_DATE } from "../../constants/common";
 
 export const getTimestamp = (tileId: number | string) => {
-    const tileDate = tileId !== "placeholder" && new Date(CHECK_DATE.year, CHECK_DATE.month, tileId as number);
+    const tileDate = tileId !== "placeholder" && new Date(CHECK_DATE.promoStage1.year, CHECK_DATE.promoStage1.month, tileId as number);
     
     return tileDate && tileDate.getTime();
 };
